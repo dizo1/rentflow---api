c5133a4aecb2837b4cb66317a5b66915801e5170
Muganda <dizomuganda@ymail.com>
2026-06-02 09:24:45 +0000

    tab spacd  resources

--- config/routes.rb
+++ config/routes.rb
@@ -55,20 +55,20 @@ Rails.application.routes.draw do
       end
 
      # Tenants
-  resources :tenants, only: [:index, :show, :create, :update, :destroy] do
-    member do
-      patch :assign
-    end
-  end
+      resources :tenants, only: [:index, :show, :create, :update, :destroy] do
+        member do
+          patch :assign
+        end
+      end
 
-  resources :units, only: [:show, :update, :destroy] do
-  resources :rent_records, only: [:index, :create]
-  resources :maintenance_logs, only: [:index, :create]
-  resource :tenant, only: [:show, :create], controller: 'tenants', action: { show: :show_by_unit, create: :create_for_unit }
-  collection do
-    get :vacant
-  end
-  end
+      resources :units, only: [:show, :update, :destroy] do
+      resources :rent_records, only: [:index, :create]
+      resources :maintenance_logs, only: [:index, :create]
+      resource :tenant, only: [:show, :create], controller: 'tenants', action: { show: :show_by_unit, create: :create_for_unit }
+      collection do
+        get :vacant
+      end
+      end
 
       # Reminders
       resources :reminders, only: [:index, :show, :create, :update, :destroy]
